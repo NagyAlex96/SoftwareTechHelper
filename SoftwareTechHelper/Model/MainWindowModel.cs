17f3a5f7bea1dcaf3bcc4491871a0bbaefdc207f
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-08-20 19:05:39 +0200

    MainWindow model created

---
+++ SoftwareTechHelper/Model/MainWindowModel.cs
@@ -0,0 +1,65 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SoftwareTechHelper.Model
+{
+    class MainWindowModel : ObservableObject
+    {
+        private double windowHeight = SystemParameters.PrimaryScreenHeight / 1.5;
+        public double WindowHeight
+        {
+            get { return windowHeight; }
+            set
+            {
+                windowHeight = value;
+                RaisePropertyChanged(() => this.WindowHeight);
+            }
+        }
+
+        private double windowWidth = SystemParameters.PrimaryScreenWidth / 1.5;
+        public double WindowWidth
+        {
+            get { return windowWidth; }
+            set
+            {
+                windowWidth = value;
+                RaisePropertyChanged(() => this.WindowWidth);
+            }
+        }
+
+        private WindowState curWindowState;
+        public WindowState CurWindowState
+        {
+            get
+            {
+                return curWindowState;
+            }
+            set
+            {
+                curWindowState = value;
+                RaisePropertyChanged(() => this.CurWindowState);
+            }
+        }
+
+        private ResizeMode resizing;
+        public ResizeMode Resizing
+        {
+            get
+            {
+                return resizing;
+            }
+            set
+            {
+                resizing = value;
+                RaisePropertyChanged(() => this.Resizing);
+            }
+        }
+
+
+    }
+}
