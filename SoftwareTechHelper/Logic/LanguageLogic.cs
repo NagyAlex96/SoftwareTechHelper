32ff8140bd13b8d2e92104295a93550dfc7e9913
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-08-20 19:05:08 +0200

    LanguageLogic created

---
+++ SoftwareTechHelper/Logic/LanguageLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SoftwareTechHelper.Logic
+{
+    internal class LanguageLogic : ILanguageLogic
+    {
+        public string Root
+        {
+            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
+        }
+
+        public ResourceDictionary SetupLanguage(string Language)
+        {
+            ResourceDictionary dictionary = new ResourceDictionary();
+
+            dictionary.Source = new Uri(Root + $@"\Assest\Localization\{Language}.xaml", UriKind.RelativeOrAbsolute);
+
+            return dictionary;
+        }
+    }
+}
